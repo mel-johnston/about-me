91f553d68bb5f3886038366205e06b046f3c544f
MELANIE JOHNSTON <MELANIE.JOHNSTON.1@OUTLOOK.COM>
2022-11-30 22:48:24 +0000

    lab 3 first draft js

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # about-me
 
-Lab 02 
+## Lab 02 
+
+Basic HTML/CSS set-up
+JS - prompts and alert responses
+
+## Lab 03 Instructions
+
+<!-- - Create a “Top Ten” at the bottom of your HTML page as an ordered list in HTML. 
+- Convert your work experience and education summary into an unordered list using HTML -->
+
+- Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+- Indicates through an alert if the guess is “too high” or “too low”.
+- It should give the user exactly four opportunities to get the correct answer.
+- After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
 
 
-Author: Melanie Johnston
+
+
+
+- Add a 7th question that has multiple possible correct answers that are stored in an array.
+- Give the user 6 attempts to guess the correct answer.
+- The guesses will end once the user guesses a correct answer or they run out of attempts.
+- Display all the possible correct answers to the user.
+- Consider using a loop of some sort for this question.
+
+- Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
+
+Continue using console longs throughout the code. 
+
+
+
+
+
+
+
+---
+
+### Links & Resources
+
+reset breaks lists - found this to fix it 
+https://stackoverflow.com/questions/12013142/lists-not-showing-bullets-in-html5-css
--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let score = 0;
+
 let userName = prompt('What is your name?');
 
 alert(`Welcome to my site ${userName}! Here's a little quiz about me. Please answer the following questions with a yes/no or y/n`);
@@ -9,6 +11,7 @@ let question1Guess = prompt('Am I a CodeFellows student?').toLowerCase();
 if (question1Guess === 'yes' || question1Guess === 'y') {
   // console.log('Correct, I am a Codefellows student!');
   alert('Correct, I am a Codefellows student!');
+  score++;
 } else if (question1Guess === 'no' || question1Guess === 'n') {
   // console.log('Sorry, you got it wrong.');
   alert('Sorry, you got it wrong.');
@@ -19,6 +22,7 @@ let question2Guess = prompt('Do I have previous programming experience?').toLowe
 if (question2Guess === 'no' || question2Guess === 'n') {
   // console.log('Correct, I do not have any previous experience.');
   alert('Correct, I do not have any previous experience.');
+  score++;
 } else if (question2Guess === 'yes' || question2Guess === 'y') {
   // console.log('Sorry, you got it wrong.');
   alert('Sorry, you got it wrong.');
@@ -27,8 +31,9 @@ if (question2Guess === 'no' || question2Guess === 'n') {
 let question3Guess = prompt('Do I live in Texas?').toLowerCase();
 
 if (question3Guess === 'yes' || question3Guess === 'y') {
-//  console.log('Yep, I live in Dallas, Texas.');
+  //  console.log('Yep, I live in Dallas, Texas.');
   alert('Yep, I live in Dallas, Texas.');
+  score++;
 } else if (question3Guess === 'no' || question3Guess === 'n') {
   // console.log('Sorry, you got it wrong.');
   alert('Sorry, you got it wrong.');
@@ -39,6 +44,7 @@ let question4Guess = prompt('Am I using Windows?').toLowerCase();
 if (question4Guess === 'yes' || question4Guess === 'y') {
   // console.log('Yes, my computer is Windows 10.');
   alert('Yes, my computer is Windows 10.');
+  score++;
 } else if (question4Guess === 'no' || question4Guess === 'n') {
   // console.log('Sorry, you got it wrong.');
   alert('Sorry, you got it wrong.');
@@ -49,9 +55,44 @@ let question5Guess = prompt('Have I traveled out of the United States?').toLower
 if (question5Guess === 'yes' || question5Guess === 'y') {
   // console.log('Good memory! My fun fact was that I went to Peru when I was a kid and ate guinea pig.');
   alert('Good memory! My fun fact was that I went to Peru when I was a kid and ate guinea pig.');
+  score++;
 } else if (question5Guess === 'no' || question5Guess === 'n') {
   // console.log('Sorry, you got it wrong.');
   alert('Sorry, you got it wrong.');
 }
 
-alert(`Well, ${userName} thanks for playing! Feel free to have a look around.`);
+//lab 3 below
+
+let myNum = '29';
+
+for (let i = 1; i <= 4; i++) {
+  let question6Guess = prompt('What number am I thinking of?');
+  if (question6Guess === myNum) {
+    alert('You got it!');
+    score++;
+    break;
+  } else if (question6Guess < myNum) {
+    alert('Try higher!');
+  } else if (question6Guess > myNum) {
+    alert('Try lower!');
+  }
+}
+
+
+for (let i = 1; i <= 6; i++) {
+  const faveFood = ['burrito', 'pizza', 'bread', 'pasta', 'marshmallows'];
+  let question7Guess = prompt('What is one of my favorite foods?').toLowerCase();
+  if (faveFood.indexOf(question7Guess) !== -1) {
+    alert(`You got it! Here's the rest: ${faveFood}.`);
+    score++;
+    break;
+  } else if (faveFood.indexOf(question7Guess) === -1 && i < 6) {
+    alert('Try again!');
+  } else {
+    alert(`Sorry no more tries for you. Here they are: ${faveFood}.`);
+  }
+}
+
+alert(`Congratulations, ${userName}, you're finally done! Your score is ${score}.`);
+
+
