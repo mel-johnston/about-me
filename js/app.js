482f0d33dbdf89e1835793f466bc7ee8cf4c0bbd
stevengant <sgant39@gmail.com>
2022-12-01 21:42:46 +0000

    pair programming

--- js/app.js
+++ js/app.js
@@ -8,90 +8,117 @@ alert(`Welcome to my site ${userName}! Here's a little quiz about me. Please ans
 
 let question1Guess = prompt('Am I a CodeFellows student?').toLowerCase();
 
-if (question1Guess === 'yes' || question1Guess === 'y') {
-  // console.log('Correct, I am a Codefellows student!');
-  alert('Correct, I am a Codefellows student!');
-  score++;
-} else if (question1Guess === 'no' || question1Guess === 'n') {
-  // console.log('Sorry, you got it wrong.');
-  alert('Sorry, you got it wrong.');
+function questionOne() {
+  if (question1Guess === 'yes' || question1Guess === 'y') {
+    // console.log('Correct, I am a Codefellows student!');
+    alert('Correct, I am a Codefellows student!');
+    score++;
+  } else if (question1Guess === 'no' || question1Guess === 'n') {
+    // console.log('Sorry, you got it wrong.');
+    alert('Sorry, you got it wrong.');
+  }
 }
 
+questionOne();
+
+function questionTwo() {
 let question2Guess = prompt('Do I have previous programming experience?').toLowerCase();
 
-if (question2Guess === 'no' || question2Guess === 'n') {
-  // console.log('Correct, I do not have any previous experience.');
-  alert('Correct, I do not have any previous experience.');
-  score++;
-} else if (question2Guess === 'yes' || question2Guess === 'y') {
-  // console.log('Sorry, you got it wrong.');
-  alert('Sorry, you got it wrong.');
+  if (question2Guess === 'no' || question2Guess === 'n') {
+    // console.log('Correct, I do not have any previous experience.');
+    alert('Correct, I do not have any previous experience.');
+    score++;
+  } else if (question2Guess === 'yes' || question2Guess === 'y') {
+    // console.log('Sorry, you got it wrong.');
+    alert('Sorry, you got it wrong.');
+  }
 }
 
+questionTwo();
+
+function questionThree() {
 let question3Guess = prompt('Do I live in Texas?').toLowerCase();
 
-if (question3Guess === 'yes' || question3Guess === 'y') {
-  //  console.log('Yep, I live in Dallas, Texas.');
-  alert('Yep, I live in Dallas, Texas.');
-  score++;
-} else if (question3Guess === 'no' || question3Guess === 'n') {
-  // console.log('Sorry, you got it wrong.');
-  alert('Sorry, you got it wrong.');
+  if (question3Guess === 'yes' || question3Guess === 'y') {
+    //  console.log('Yep, I live in Dallas, Texas.');
+    alert('Yep, I live in Dallas, Texas.');
+    score++;
+  } else if (question3Guess === 'no' || question3Guess === 'n') {
+    // console.log('Sorry, you got it wrong.');
+    alert('Sorry, you got it wrong.');
+  }
 }
 
+questionThree();
+
+function questionFour() {
 let question4Guess = prompt('Am I using Windows?').toLowerCase();
 
-if (question4Guess === 'yes' || question4Guess === 'y') {
-  // console.log('Yes, my computer is Windows 10.');
-  alert('Yes, my computer is Windows 10.');
-  score++;
-} else if (question4Guess === 'no' || question4Guess === 'n') {
-  // console.log('Sorry, you got it wrong.');
-  alert('Sorry, you got it wrong.');
+  if (question4Guess === 'yes' || question4Guess === 'y') {
+    // console.log('Yes, my computer is Windows 10.');
+    alert('Yes, my computer is Windows 10.');
+    score++;
+  } else if (question4Guess === 'no' || question4Guess === 'n') {
+    // console.log('Sorry, you got it wrong.');
+    alert('Sorry, you got it wrong.');
+  }
 }
 
+questionFour();
+
+function questionFive() {
+  
 let question5Guess = prompt('Have I traveled out of the United States?').toLowerCase();
 
-if (question5Guess === 'yes' || question5Guess === 'y') {
-  // console.log('Good memory! My fun fact was that I went to Peru when I was a kid and ate guinea pig.');
-  alert('Good memory! My fun fact was that I went to Peru when I was a kid and ate guinea pig.');
-  score++;
-} else if (question5Guess === 'no' || question5Guess === 'n') {
-  // console.log('Sorry, you got it wrong.');
-  alert('Sorry, you got it wrong.');
+  if (question5Guess === 'yes' || question5Guess === 'y') {
+    // console.log('Good memory! My fun fact was that I went to Peru when I was a kid and ate guinea pig.');
+    alert('Good memory! My fun fact was that I went to Peru when I was a kid and ate guinea pig.');
+    score++;
+  } else if (question5Guess === 'no' || question5Guess === 'n') {
+    // console.log('Sorry, you got it wrong.');
+    alert('Sorry, you got it wrong.');
+  }
 }
+
+questionFive();
 
 //lab 3 below
-
+function questionSix() {
 let myNum = '29';
 
-for (let i = 1; i <= 4; i++) {
-  let question6Guess = prompt('What number am I thinking of?');
-  if (question6Guess === myNum) {
-    alert('You got it!');
-    score++;
-    break;
-  } else if (question6Guess < myNum) {
-    alert('Try higher!');
-  } else if (question6Guess > myNum) {
-    alert('Try lower!');
+  for (let i = 1; i <= 4; i++) {
+    let question6Guess = prompt('What number am I thinking of?');
+    if (question6Guess === myNum) {
+      alert('You got it!');
+      score++;
+      break;
+    } else if (question6Guess < myNum) {
+      alert('Try higher!');
+    } else if (question6Guess > myNum) {
+      alert('Try lower!');
+    }
   }
 }
 
+questionSix();
 
-for (let i = 1; i <= 6; i++) {
-  const faveFood = ['burrito', 'pizza', 'bread', 'pasta', 'marshmallows'];
-  let question7Guess = prompt('What is one of my favorite foods?').toLowerCase();
-  if (faveFood.indexOf(question7Guess) !== -1) {
-    alert(`You got it! Here's the rest: ${faveFood}.`);
-    score++;
-    break;
-  } else if (faveFood.indexOf(question7Guess) === -1 && i < 6) {
-    alert('Try again!');
-  } else {
-    alert(`Sorry no more tries for you. Here they are: ${faveFood}.`);
+function questionSeven() {
+  for (let i = 1; i <= 6; i++) {
+    const faveFood = ['burrito', 'pizza', 'bread', 'pasta', 'marshmallows'];
+    let question7Guess = prompt('What is one of my favorite foods?').toLowerCase();
+      if (faveFood.indexOf(question7Guess) !== -1) {
+        alert(`You got it! Here's the rest: ${faveFood}.`);
+        score++;
+        break;
+      } else if (faveFood.indexOf(question7Guess) === -1 && i < 6) {
+        alert('Try again!');
+      } else {
+        alert(`Sorry no more tries for you. Here they are: ${faveFood}.`);
+      }
   }
 }
+
+questionSeven();
 
 alert(`Congratulations, ${userName}, you're finally done! Your score is ${score}.`);
 
